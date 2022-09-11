2ec262112a3de22eeb123036dc761faddb9d7364
esmaeilifardi <esmaeilifardi@gmail.com>
2022-09-11 19:32:07 +0200

    dele print

--- dadekavi_editetd_for_pipline.py
+++ dadekavi_editetd_for_pipline.py
@@ -78,7 +78,6 @@ df_num = df.drop('ocean_proximity', axis = 1)            # jense dade fargh dara
 #-----------------------------------------------------missing dist matrix---------------------------------------------
 
 msno.matrix(df)
-print()
 plt.show()
 
 #------method 1 for missing data---------------------
